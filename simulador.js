121a3d67a16728b7e1c6a577e232d2b910392de2
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-10-25 22:14:58 -0600

    asdf

--- simulador.js
+++ simulador.js
@@ -1,6 +1,24 @@
+window.addEventListener("load", function(){
+  console.log("fuck1");
+fuck();
+
+});
+
+function fuck(){
+  let data = ["Ram", "Shyam", "Sita", "Gita"];
+  let list = document.getElementById("tipos-inversion");
+  for (i = 0; i < data.length; ++i) {
+      let li = document.createElement('li');
+      li.innerText = data[i];
+      list.appendChild(li);
+  }
+}
+
+// Configuracion de la grafica:
 (async function() {
+  console.log("fuck2");
     const dataArray = [
-      { year: 2010, count: 10 },
+      { year: 2010, count: 13 },
       { year: 2011, count: 20 },
       { year: 2012, count: 15 },
       { year: 2013, count: 25 },
@@ -31,3 +49,7 @@
       }
     );
   })();
+
+  // Inicialización de los tipos de inversion
+
+
